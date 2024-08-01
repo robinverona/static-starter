71ab1e78355b0c36f8be9ede0d5ad02d3e331141
Robin Verona <contact@robinverona.com>
2024-08-01 12:02:56 +0200

    clean up repo

--- js/index.js
+++ js/index.js
@@ -1,9 +1,1 @@
 console.log('script loaded');
-
-var swiper = new Swiper(".mySwiper", {
-    direction: "vertical",
-    slidesPerView: 1,
-    spaceBetween: 30,
-    mousewheel: true,
-    loop: true,
-  });
